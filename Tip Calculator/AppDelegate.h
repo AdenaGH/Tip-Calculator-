11cd29ed0a2b0a7e8138caa95bcf1bfdc9b18401
Adena Rowana Ninvalle <adenan@fb.com>
2021-06-22 15:29:55 -0400

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Adena Rowana Ninvalle on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
